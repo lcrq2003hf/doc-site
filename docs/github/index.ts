d473c83fd4a83f72efbfd0f689e847ff0a15564a
liuchao <liuchao@acoinfo.com>
2023-12-06 16:51:13 +0800

    docs: 代码统计

--- docs/github/index.ts
+++ docs/github/index.ts
@@ -6,5 +6,6 @@ export const githubRoutes: DefaultTheme.SidebarItem[] = [
     { text: '关联项目至 git 中', link: '/github/connect' },
     { text: '切换 ssh 源', link: '/github/switch-config' },
     { text: 'git clone 代码缓慢', link: '/github/clone-slow' },
+    { text: '代码统计', link: '/github/statistics' },
   ] }
 ]
